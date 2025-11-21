45ab49e21a5ca17fd7060944299ec59b110f4f0d
tkv <egahaker@mail.ru>
2025-11-21 12:37:18 +0300

    generate_slug done

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, status
+from fastapi.responses import RedirectResponse
 from database.db import engine
 from database.models import Base
+from service import generate_shore_url
 
 
 @asynccontextmanager
@@ -10,13 +12,16 @@ async def lifespan(app: FastAPI):
         await connection.run_sync(Base.metadata.create_all)
     yield
 
+
 app = FastAPI(lifespan=lifespan)
 
 
 @app.post('/short_url')
-async def generate_short_url(long_url: str = Body(embed=True)):
-    return ...
+async def generate_slug(long_url: str = Body(embed=True)):
+    new_slug = await generate_shore_url(long_url)
+    return {'data': new_slug}
+
 
 @app.get('/{slug}')
 async def redirect_to_url(slug: str):
-    return ... # redirect
+    return RedirectResponse(url=..., status_code=status.HTTP_302_FOUND)
--- service.py
+++ service.py
@@ -1,1 +1,13 @@
+from database.crud import add_slug_to_database
 from shortener import generate_random_slug
+
+async def generate_shore_url(
+        long_url: str
+) -> str:
+    slug = generate_random_slug()
+    await add_slug_to_database(slug, long_url)
+    return slug
+
+
+async def get_url_by_slug():
+    pass
